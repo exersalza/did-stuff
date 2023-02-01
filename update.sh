a04e59da626b6f576d2af25f7e1c8d8132c576e1
exersalza <squidodle44@gmail.com>
2023-02-01 06:44:28 +0100

    did stuff

--- update.sh
+++ update.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ "$EUID" -ne 0 ]
+then
+  echo "Please run with sudo."
+  exit 2
+fi
+
 echo "Running update..."
 cp -pr ./templates/* /usr/share/salty-create
+cp -p main.py /usr/bin/salty-create
 echo "Updated"
